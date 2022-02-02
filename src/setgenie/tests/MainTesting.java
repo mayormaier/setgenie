34e662089d861c47f5b0bdad3e3ef2d35b61638e
mayormaier <maierandrewd@gmail.com>
2022-02-01 21:49:37 -0500

    add testing suite

---
+++ src/setgenie/tests/MainTesting.java
@@ -0,0 +1,8 @@
+package setgenie.tests;
+
+public class MainTesting {
+
+    public static void main(String[] args) {
+        TestHarness th = new TestHarness();
+    }
+}
---
+++ src/setgenie/tests/TestHarness.java
@@ -0,0 +1,110 @@
+package setgenie.tests;
+
+import setgenie.User;
+import setgenie.Workout;
+import setgenie.WorkoutElement;
+import setgenie.WorkoutSet;
+
+import java.util.*;
+
+/**
+ * @author andrewmaier
+ *
+ * Creating manual tests to validate SetGenie classes.
+ */
+public class TestHarness {
+
+    public TestHarness() {
+        testUserClass();
+        testWorkoutClass();
+    }
+
+    public void testUserClass() {
+        System.out.println("*** Testing User Class ***");
+        User u1 = new User("Bob", "Jones", "bjones@email.com" , "bobstrongpassword", "bjones1", User.ROLE.ATHLETE);
+
+        if (u1 != null){
+            System.out.println("User instance 1 created successfully!");
+            // Get all user info - testing getters
+            System.out.println("User Info: " + u1.getUserFirstName() + " " + u1.getUserLastName() + " " + u1.getUserEmail() + " " + u1.getUsername() + " " + u1.getUserPassword() + " " + u1.getUserRole() + " " + u1.getUserCreatedWorkouts() + " " + u1.getUserSavedWorkouts());
+
+            // Set new info - testing setters
+            u1.setUserFirstName("Mary");
+            u1.setUserLastName("Smith");
+            u1.setUserEmail("msmith@email.com");
+            u1.setUserPassword("msmithstrongpass");
+            u1.setUsername("msmith1");
+            u1.setUserRole(User.ROLE.COACH);
+
+            List<Workout> testList1= new ArrayList<>();
+            u1.setUserCreatedWorkouts(testList1);
+            u1.setUserSavedWorkouts(testList1);
+        } else {
+            System.out.println("Could not properly create user class");
+        }
+        User u2 = new User("Bob", "Jones", "bjones@email.com" , "bobstrongpassword", "bjones1");
+
+        if (u2 != null) {
+            System.out.println("User 2 created successfully!");
+            //test toString()
+            System.out.println(u2.toString());
+        } else {
+            System.out.println("Error creating user 2");
+        }
+    }
+
+    public void testWorkoutClass() {
+        System.out.println("*** Testing Workout Class ***");
+
+        // Creating WorkoutElements
+        WorkoutElement e1 = new WorkoutElement(5, 100, 65, WorkoutElement.STROKE.FREESTYLE);
+        WorkoutElement e2 = new WorkoutElement(5, 100, 65, WorkoutElement.STROKE.FREESTYLE, "Descend 1-5");
+        WorkoutElement e3 = new WorkoutElement(400, 300, WorkoutElement.STROKE.IM, "Drill/Kick/Drill/Swim by 25");
+
+        // Creating WorkoutSets
+        WorkoutSet s1 = new WorkoutSet("Meet Warm Up", WorkoutSet.SETTYPE.WARMUP, 1);
+        s1.addElement(e1);
+        s1.addElement(e2);
+        s1.addElement(e3);
+
+        // Creating User
+        User u1 = new User("Bob", "Jones", "bjones@email.com" , "bobstrongpassword", "bjones1", User.ROLE.ATHLETE);
+        User u2 = new User("Billy", "Johns", "bjohns@email.com" , "billystrongpassword", "bjohns1", User.ROLE.COACH);
+
+        //Testing Workout
+        Workout w1 = new Workout("Workout 1", "This is a workout test", u1);
+
+        if (w1 != null){
+            System.out.println("User instance 1 created successfully!");
+
+            //Getting workout information - testing getters
+            System.out.println("Workout info: " + w1.getWorkoutName() + " " + w1.getWorkoutNotes()  + " " + w1.getWorkoutOwner() + " " + w1.getWorkoutDistance() + " " + w1.getWorkoutDuration() + " " + w1.getUuid() + " " + w1.getWorkoutSetList() + " " + w1.getWorkoutStrokePercentages());
+
+            //Setting new information - testing setters
+            UUID testUUID = UUID.randomUUID();
+            w1.setUuid(testUUID);
+
+            w1.setWorkoutDistance(100);
+            w1.setWorkoutName("Testing Set Name");
+            w1.setWorkoutDuration(3600);
+            w1.setWorkoutNotes("setting the notes");
+            w1.setWorkoutOwner(u2);
+
+            Map<String, Double> strokesTest = new HashMap<>();
+            strokesTest.put("Freestyle", 100.0);
+            w1.setWorkoutStrokePercentages(strokesTest);
+
+
+            List<WorkoutSet> testSetList = new ArrayList();
+            testSetList.add(s1);
+            w1.setWorkoutSetList(testSetList);
+
+            // test additional methods
+            w1.addSet(s1);
+            w1.calculateMetadata();
+
+            // testing toString()
+            System.out.println(w1.toString());
+        }
+    }
+}
